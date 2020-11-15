cc0cf343cf2aa834ba4fc6541acbc385f66acd7c
iamwwc <qaq1362211689@gmail.com>
2020-11-15 11:37:24 +0800

    rust手册第二章

---
+++ src/bin/2.rs
@@ -0,0 +1,27 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+fn main() {
+    println!("Guess your number!");
+    let mut secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}",secret_number);
+    loop {
+        println!("Please input your number");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line!");
+        println!("Your guess number is {}",guess);
+        println!("You guessed: {}", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You wil!");
+                break;
+            }
+        }
+    }
+}
